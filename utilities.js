6291828b0ee97a19e1d3af21e705c5cdf59a4028
fndcsonlinerepo <fndcsonline@gmail.com>
2025-03-03 07:01:57 +0400

    Update utilities.js

--- utilities.js
+++ utilities.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the first language link
+    var firstLang = document.querySelector(".FN_Lang-Selector-Link");
+
+    if (firstLang) {
+        // Get the text content of the first language
+        var firstLangText = firstLang.textContent.trim();
+
+        // Set the toggle text to the first language
+        var toggleText = document.querySelector(".FN_Dropdown-Toggle-Text");
+        if (toggleText) {
+            toggleText.textContent = firstLangText;
+        }
+    }
+});
 
