1d5a95083af7ea92447db7c055d7efba12fcffc1
fndcsonlinerepo <fndcsonline@gmail.com>
2025-03-03 07:01:30 +0400

    Update language.js

--- language.js
+++ language.js
@@ -88,19 +88,4 @@ async function translateText(text, targetLang) {
     let data = await response.json();
     return data.translatedText;
 }
-document.addEventListener("DOMContentLoaded", function() {
-    // Select the first language link
-    var firstLang = document.querySelector(".FN_Lang-Selector-Link");
-
-    if (firstLang) {
-        // Get the text content of the first language
-        var firstLangText = firstLang.textContent.trim();
-
-        // Set the toggle text to the first language
-        var toggleText = document.querySelector(".FN_Dropdown-Toggle-Text");
-        if (toggleText) {
-            toggleText.textContent = firstLangText;
-        }
-    }
-});
 
