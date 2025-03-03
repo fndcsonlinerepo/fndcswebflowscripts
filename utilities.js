621208703894d5b4b308ab53756642182cc26077
fndcsonlinerepo <fndcsonline@gmail.com>
2025-03-03 22:53:30 +0400

    utilities.js

--- utilities.js
+++ utilities.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Select the first language link
     var firstLang = document.querySelector(".FN_Lang-Selector-Link");
 
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // Get the text content of the first language
         var firstLangText = firstLang.textContent.trim();
 
-        // Set the toggle text to the first language
-        var toggleText = document.querySelector(".FN_Dropdown-Toggle-Text");
+        // Set the text inside the dropdown toggle
+        var toggleText = document.querySelector(".FN_Dropdown-Toggle-Text"); 
         if (toggleText) {
             toggleText.textContent = firstLangText;
         }
